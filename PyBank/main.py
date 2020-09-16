2e1d8da822660c2e22bb2a1c269d5c7aeae31af8
Adam Katz <adamkatz@Adams-MacBook-Pro-7.local>
2020-09-16 12:37:51 -0500

    Prints total months.

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,17 +1,22 @@
 import os
 import csv
-csvpath = os.path.join('..', 'Resources', 'budget_data.csv')
+csvpath = os.path.join("Resources", "budget_data.csv")
 Date = []
 ProfitLoss = []
 
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
     for row in csvreader:
-    Date.append(row[1])
-    ProfitLoss.append(row[2])
-    monthCount=Date.count(row[1]-1)
+        Date.append(row[0])
+        ProfitLoss.append(row[1])
+        totMonth = len(Date)-1
+        
+    print("Total months: " + str(totMonth))
 
-print("Total months: ", monthCount)
+  
+
+
+    
       
     
 
