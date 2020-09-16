7c193c992672763ac0a3bd19317109f990577d53
Adam Katz <adamkatz@Adams-MacBook-Pro-7.local>
2020-09-16 14:40:52 -0500

    calculates and presents the total net amount of money

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,20 +3,15 @@ import csv
 csvpath = os.path.join("Resources", "budget_data.csv")
 Date = []
 ProfitLoss = []
-
+total = 0
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
+    next(csvreader, None)
     for row in csvreader:
         Date.append(row[0])
-        ProfitLoss.append(row[1])
-        totMonth = len(Date)-1
-        
+        totMonth = len(Date)
+        ProfitLoss.append(row[1]) 
+        total += int(row[1])
+    
     print("Total months: " + str(totMonth))
-
-  
-
-
-    
-      
-    
-
+    print("Total: $" + str(total))
