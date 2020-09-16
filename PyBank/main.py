1575756ed96c2f091d2ee6ed9ddb923b56d7850f
Adam Katz <adamkatz@Adams-MacBook-Pro-7.local>
2020-09-16 11:31:07 -0500

    makes path for csv and begins to append it to different lists

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,3 +1,12 @@
 import os
 import csv
-csvpath = os.path.join('..', 'Resources', 'Budget_data.csv')
+csvpath = os.path.join('..', 'Resources', 'budget_data.csv')
+Date = []
+ProfitLoss = []
+
+with open(csvpath) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+    For row in csvreader:
+        Date.append(row[1])
+        ProfitLoss.append(row[2])
+    
