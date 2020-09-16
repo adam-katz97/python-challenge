6f8c00a7dc37bb043321a2e3b9d0ff16646b7a8c
Adam Katz <adamkatz@Adams-MacBook-Pro-7.local>
2020-09-16 12:02:06 -0500

    attempts to find and print total amount of months

--- PyBank/main.py
+++ PyBank/main.py
@@ -5,8 +5,13 @@ Date = []
 ProfitLoss = []
 
 with open(csvpath) as csvfile:
-    csvreader = csv.reader(csvfile, delimiter=',')
-    For row in csvreader:
-        Date.append(row[1])
-        ProfitLoss.append(row[2])
+    csvreader = csv.reader(csvfile, delimiter=",")
+    for row in csvreader:
+    Date.append(row[1])
+    ProfitLoss.append(row[2])
+    monthCount=Date.count(row[1]-1)
+
+print("Total months: ", monthCount)
+      
     
+
