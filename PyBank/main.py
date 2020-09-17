d0c08135d65c41a438856e4712cc9cf11a48a769
Adam Katz <adamkatz@Adams-MacBook-Pro-7.local>
2020-09-17 10:48:19 -0500

    adds folders for the text file that will be made in the alaysis

--- PyBank/main.py
+++ PyBank/main.py
@@ -4,14 +4,33 @@ csvpath = os.path.join("Resources", "budget_data.csv")
 Date = []
 ProfitLoss = []
 total = 0
+plOld = 0
+plNew = 0
+average = plNew-plOld
+moneyChange = []
+totChange = 0
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
     next(csvreader, None)
     for row in csvreader:
         Date.append(row[0])
         totMonth = len(Date)
-        ProfitLoss.append(row[1]) 
+        ProfitLoss.append(row[1])
+        if ProfitLoss.count(ProfitLoss) > 1:
+            plOld = plNew
+            plNew = int(row[1])
+            moneyChange.append(average)
+            
+        else:
+            plNew = int(row[1])
+            
+        
         total += int(row[1])
-    
+    for row in moneyChange:
+        totChange += row[0]
+    print(totChange)
     print("Total months: " + str(totMonth))
     print("Total: $" + str(total))
+    print("Average change: $" + str(average))
+
+    
