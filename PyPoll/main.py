9443a41a3775e4fac8b44d1a6296c99c064a75ed
Adam Katz <adamkatz@Adams-MacBook-Pro-7.local>
2020-09-17 11:34:52 -0500

    finds and prints total amount of votes.

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,1 +1,18 @@
 import os
+import csv
+csvpath = os.path.join("Resources", "election_data.csv")
+voter_ID = []
+county = []
+candidate = []
+
+with open(csvpath) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+    next(csvreader, None)
+    for row in csvreader:
+        voter_ID.append(row[0])
+        county.append(row[1])
+        candidate.append(row[2])
+        totVote = len(voter_ID)
+    print("Election Results")
+    print("-----------------------------------------")
+    print('total votes: ', totVote)
