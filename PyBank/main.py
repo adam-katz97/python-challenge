8af6d6d327e7344c154df658367e92f38ece0ed8
Adam Katz <adamkatz@Adams-MacBook-Pro-7.local>
2020-09-18 10:03:35 -0500

    Displays dates for the max increase and decrease of profits.

--- PyBank/main.py
+++ PyBank/main.py
@@ -9,7 +9,8 @@ maxLoss = 0
 prevValue = 0
 moneyChange = []
 totChange = 0
-
+gainDate = "Waiting input"
+lossDate = "waiting input"
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
     next(csvreader, None)
@@ -20,8 +21,10 @@ with open(csvpath) as csvfile:
         change = int(row[1]) - prevValue
         if change > 0 and change > maxGain:
             maxGain = change
+            gainDate = str(row[0])
         if change < 0 and change < maxLoss:
             maxLoss = change
+            lossDate = str(row[0])
         total += int(row[1])
         
     final_day = int(ProfitLoss[-1])
@@ -30,10 +33,9 @@ with open(csvpath) as csvfile:
     moneyTime = dict(zip(Date, ProfitLoss))
 
     
-    print max(ProfitLoss)
     print("Total months: " + str(totMonth))
     print("Total: $" + str(total))
     print("Average change: $" + str(average))
-    print("Greatest increase: " + str(maxGain))
-    print("Greatest decrease: " + str(maxLoss))
+    print("Greatest Increase in Profits: " + gainDate + "  $" + str(maxGain))
+    print("Greatest Decrease in Profits: " + lossDate + "  $" + str(maxLoss))
     
