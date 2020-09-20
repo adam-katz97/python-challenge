27065f4b10b42a4a35e8d1375d16133addda902a
Adam Katz <adamkatz@Adams-MacBook-Pro-7.local>
2020-09-20 12:01:56 -0500

    finds and prints the candidates names, the percentage of votes they have and how many votes they have.

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,18 +1,38 @@
 import os
 import csv
-csvpath = os.path.join("Resources", "election_data.csv")
+csvpath = os.path.join('Resources', 'election_data.csv')
 voter_ID = []
 county = []
 candidate = []
+result = []
+totVote = 0
+vote_num = 0
+percenCount = {"name": "percentage"}
+voteCount = {"name": "votes"}
 
 with open(csvpath) as csvfile:
+    
     csvreader = csv.reader(csvfile, delimiter=",")
     next(csvreader, None)
+    
     for row in csvreader:
         voter_ID.append(row[0])
         county.append(row[1])
         candidate.append(row[2])
         totVote = len(voter_ID)
+    
+    result = set(candidate)
+    
+
+
+      
+    
+        
+
+
+
     print("Election Results")
     print("-----------------------------------------")
-    print('total votes: ', totVote)
+    print('Total Votes: ', totVote)
+    for i in result:
+        print(i + ": " + str(round(((candidate.count(i)/totVote)*100), 2))+ "% " + str(candidate.count(i)))
